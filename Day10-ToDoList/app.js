81b2aede94dbac77f383feae8455871f48fc7c6d
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-10-12 20:15:00 +0530

    index elements created with the h1, input, button, h2 and input class

---
+++ Day10-ToDoList/app.js
@@ -0,0 +1,8 @@
+// Pseudo Code
+
+// user put the input data and click on the add button
+// the tasks come at the checkbox and we can check it off once tasks are complete
+
+// logic
+// we put the data in the input-data and button add click 
+// append the list in the task list with the checkbox
