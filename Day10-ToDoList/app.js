e5db85071f8d15305ef8b237fd00046bfa80e1c1
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-10-13 23:54:40 +0530

    logic made in the js with the delete button in the html

--- Day10-ToDoList/app.js
+++ Day10-ToDoList/app.js
@@ -6,3 +6,39 @@
 // logic
 // we put the data in the input-data and button add click 
 // append the list in the task list with the checkbox
+// clear button will clear the tasks
+
+const inputEl = document.querySelector('.input-data');
+const addBtn = document.querySelector('.add-btn');
+const resultEl = document.querySelector('.result')
+const deleteBtn = document.querySelector('.delete')
+
+// logic
+
+function render(event) {
+  event.preventDefault();
+  let inputData = inputEl.value;
+
+  // creating the checkbox elements
+  const container = document.createElement('div');
+  container.classList.add('container')
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox'
+
+  const list = document.createElement('span')
+  list.textContent = inputData
+
+  container.appendChild(checkbox)
+  container.appendChild(list)
+
+  resultEl.appendChild(container)
+}
+
+function refresh() {
+  
+  resultEl.innerHTML = ''
+}
+
+addBtn.addEventListener('click', render);
+deleteBtn.addEventListener('click', refresh)
