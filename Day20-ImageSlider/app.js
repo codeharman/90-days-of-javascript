37e96d7ff948bcc4e8969316d0d3db45fd3d165d
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-10-30 20:53:38 +0530

    html updated with the contact page, styling added, logic made on the js

--- Day20-ImageSlider/app.js
+++ Day20-ImageSlider/app.js
@@ -2,10 +2,46 @@
 
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
-
-// container
-
 const container = document.querySelectorAll('.slider');
 
-// logic
-// 
+let count = 0;
+
+// container
+// Pseudo code
+// .slider is showing the picture
+// when the person clicks on the > next emoji comes up
+// when the user clicks on the < previous emoji comes up
+// if the user reaches at the end nothing happense after that
+
+// Image component
+
+function image() {
+  const container = document.querySelectorAll('.slider');
+  for (let i = 0; i < container.length; i++) {
+
+    // out put for the console.log was 0,1,2 that means for loop is working
+    //  console.log(i)
+
+    container[i].style.display = "none";
+  } 
+  container[count].style.display = "block";
+}
+
+// Calling to show the image
+image()
+
+// right button logic 
+
+right.addEventListener('click', () => {
+  if (count < container.length - 1) {
+    count++;
+    image()
+  }
+})
+
+left.addEventListener('click', () => {
+  if (count > 0) {
+    count--
+    image()
+  }
+})
